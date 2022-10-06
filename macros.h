79a270dd41d0b4ecde04aa2e44256031fb4be720
christianadouglas3842 <105051226+christianadouglas3842@users.noreply.github.com>
2022-10-06 12:17:34 +0300

    Update macros.h

--- macros.h
+++ macros.h
@@ -8,7 +8,6 @@
 //
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
@@ -22,7 +21,6 @@
 #define DEVICE_CHECK(call)                                           \
   if ((call) != GetDeviceSuccess()) {                                \
     throw std::runtime_error(                                        \
-        #call " API call failed: " + GetLastErrorString() + " at " + \
         __FILE__ + ", line" + std::to_string(__LINE__));             \
   }
 
